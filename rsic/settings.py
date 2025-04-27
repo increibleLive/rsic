32d9a58f196afe7fbe7a13e0d158091cc02ae516
increibleLive <62342064+increibleLive@users.noreply.github.com>
2025-04-27 14:57:21 +0530

    Add files via upload

--- rsic/settings.py
+++ rsic/settings.py
@@ -80,10 +80,21 @@ WSGI_APPLICATION = 'rsic.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'kvUWKIeSUooTvuhItjGGbKzSWoMvwmFA',
+        'HOST': 'shinkansen.proxy.rlwy.net',
+        'PORT': '47313',
     }
 }
 
